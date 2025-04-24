c1da57c53ea87652020d899656a99328e4054402
Shreyansh Shukla <shreyansh.14010@gmail.com>
2025-04-24 17:17:35 +0530

    init server

--- apps/ws-backend/src/app.ts
+++ apps/ws-backend/src/app.ts
@@ -1,3 +1,10 @@
 import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer();
+
+wss.on("connection", (ws: WebSocket) => {
+    console.log("Client connected");
+    ws.on("message", (message) => {
+        console.log("Message received: ", message);
+    });
+});
