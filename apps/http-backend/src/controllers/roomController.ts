6eeaf368106066fe7abb1ce0f232e3da03411765
Shreyansh Shukla <shreyansh.14010@gmail.com>
2025-04-25 19:10:26 +0530

    updated room controller

--- apps/http-backend/src/controllers/roomController.ts
+++ apps/http-backend/src/controllers/roomController.ts
@@ -1,15 +1,48 @@
 import { Request, Response } from "express";
 import { createRoomSchmea } from "@repo/common/types";
+import { prisma } from "@repo/db/client";
 
-export const roomController = (req: Request, res: Response) => {
-  const data = createRoomSchmea.safeParse(req.body);
-  if (!data.success) {
-    res.status(500).json({
-      msg: "invalid room credentials",
+export const roomController = async (req: Request, res: Response) => {
+  const parsedData = createRoomSchmea.safeParse(req.body);
+  try {
+    if (!parsedData.success) {
+      res.status(500).json({
+        msg: "invalid room credentials",
+      });
+      return;
+    }
+
+    const room = await prisma.room.findFirst({
+      where: {
+        slug: parsedData.data.roomName,
+      },
     });
-    return;
+
+    if (room) {
+      res.status(500).json({
+        msg: "room with this name already exists",
+      });
+      return;
+    }
+
+    const userId = req.userId;
+    if (!userId) {
+      return;
+    }
+    const createdRoom = await prisma.room.create({
+      data: {
+        slug: parsedData.data.roomName,
+        adminId: userId,
+      },
+    });
+    res.json({
+      roomId: createdRoom.id,
+    });
+  } catch (err) {
+    console.error("Error creating room:", err);
+    res.status(500).json({
+      msg: "error creating room",
+      error: err instanceof Error ? err.message : err,
+    });
   }
-  res.json({
-    roomId: "123",
-  });
 };
